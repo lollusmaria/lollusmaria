9cdc7dc73682c60b18523583df5e91b41c4537d4
lollusmaria <127745165+lollusmaria@users.noreply.github.com>
2023-03-14 11:32:22 +0100

    Add files via upload

--- etaifelse.c
+++ etaifelse.c
@@ -6,12 +6,17 @@ int main()
    printf ("inserisci la tua età");
    scanf ("%d", &età);
 
-   if (età >= 18)
+   if (età > 18)
    {
-    printf("sei maggiorenne\n");
+    printf("devi pagare le tasse\n");
    }   
+   else if (età == 18)
+   {
+    printf("auguri\n");
+    printf("oppre hai un po più di diciotto anni");
+   }
    else
    {
-    printf("non sei maggiorenne\n");
+    printf("non devi pagare le tasse\n");
    }
 }
