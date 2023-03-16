b33e19fcadee88253a601092d0bc0aaf6f2b8314
lollusmaria <127745165+lollusmaria@users.noreply.github.com>
2023-03-16 09:58:41 +0100

    Add files via upload

---
+++ nazivalla.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main ()
+{
+  int x;
+  int i = 0;
+  int n;
+  int maggiore;
+  int minore;
+  
+  printf ("inserisci un numero\n");
+  scanf("%d", &n);
+  int array[n];
+  
+  
+
+  while ( i < n)
+  {
+    printf("Inserisci un numero\n");
+    scanf("%d", &x);
+    if ( i == 0)
+    {
+     maggiore = x;
+     minore = x;
+    }
+   
+    array[i] = x;
+    i++;
+    if ( x > maggiore)
+    {
+        maggiore = x;
+    }
+
+    if ( x < minore )
+    {
+        minore = x;
+    }
+  }
+  printf("Il numero maggiore è %d\n", maggiore);
+  printf("Il numero minore è %d\n", minore);
+
+  }
